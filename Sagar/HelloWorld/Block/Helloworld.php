3262a74dc38ecb055a6c5afd730c1e028765ad87
sgrspkt <sgr.sapkota5@gmail.com>
2017-04-03 23:48:54 +0545

    initial commit

---
+++ Sagar/HelloWorld/Block/Helloworld.php
@@ -0,0 +1,11 @@
+<?php 
+namespace Sagar\HelloWorld\Block;
+
+class Helloworld extends \Magento\Framework\View\Element\Template
+{
+	public function getHelloWorldText(){
+		return "HELLO WORLD";
+	}
+}
+
+?>
---
+++ Sagar/HelloWorld/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Sagar_HelloWorld',
+    __DIR__
+);
